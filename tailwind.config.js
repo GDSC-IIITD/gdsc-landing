f5bfbbc91ab694159c1857baaa8875d3d413e3b6
Vishnu Shon <88622653+VisShon@users.noreply.github.com>
2022-09-26 17:09:22 +0530

    tailwind init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 
-
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'googleRed': "#1178D7",
+      'googleBlue': "#008A61",
+      'googleGreen': "#F8F8F8",
+      'googleYellow': "#DBDBDB",
+      'textMain': "#1E1E1E",
+      'textSecondary': "#777777",
+    },
+    fontFamily: {
+      'google': ['Open-Sans','sans-serif'],
+    },
+    screens: {
+      'small':{
+        'max':'800px'
+      },
+      'large':{
+        'min':'800px'
+      }
+    },
   },
   plugins: [],
 }
