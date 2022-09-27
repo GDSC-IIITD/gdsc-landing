1642f908521e96c45d102caeb9a4d98226856ed6
kritarth384 <kritarth20384@iiitd.ac.in>
2022-09-27 10:35:38 +0530

    Update the valueProposition page

--- src/components/sections/ValueProposition.jsx
+++ src/components/sections/ValueProposition.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import circleShape from "../../images/backgrounds/value_Proposition_svgs/proposition_circle_shape.svg";
+import waveShape from "../../images/backgrounds/value_Proposition_svgs/proposition_wave_shape.svg";
+import compShape1 from "../../images/backgrounds/value_Proposition_svgs/comp_f.svg";
+import compShapeDet1 from "../../images/backgrounds/value_Proposition_svgs/comp_f_det.svg";
+import compShape2 from "../../images/backgrounds/value_Proposition_svgs/comp_s.svg";
+import compShapeDet2 from "../../images/backgrounds/value_Proposition_svgs/comp_s_det.svg";
+import compShape3 from "../../images/backgrounds/value_Proposition_svgs/comp_t.svg";
+import compShapeDet3 from "../../images/backgrounds/value_Proposition_svgs/comp_t_det.svg";
+import compShape4 from "../../images/backgrounds/value_Proposition_svgs/comp_fo.svg";
+import compShapeDet4 from "../../images/backgrounds/value_Proposition_svgs/comp_fo_det.svg";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 function ValueProposition() {
+  useEffect(() => {
+    Aos.init({
+      offset: 300,
+      duration: 400,
+    });
+  }, []);
   return (
-    <div>ValueProposition</div>
-  )
+    <>
+      <div className="flex w-screen fixed top-10 justify-between z-0">
+        <img src={circleShape} alt="circle shape" className="mt-[1vw]" />
+        <img src={waveShape} alt="wave shape" className="h-[57rem]" />
+      </div>
+
+      <div className="z-1 w-screen grid grid-cols-1 gap-10 mt-[31px]">
+        <div className="flex flex-wrap justify-evenly items-center">
+          <div
+            className="flex items-center justify-center shrink  "
+            data-aos="slide-right"
+          >
+            <img src={compShape1} alt="component 1" />
+          </div>
+          <div
+            className="flex items-center justify-start shrink"
+            data-aos="slide-left"
+          >
+            <img src={compShapeDet1} alt="component 2" />
+          </div>
+        </div>
+
+        <div className="flex flex-wrap justify-evenly items-center">
+          <div
+            className="flex items-center justify-center shrink "
+            data-aos="fade-right"
+          >
+            <img src={compShape2} alt="component 1" />
+          </div>
+          <div
+            className="flex items-center justify-start shrink"
+            data-aos="fade-left"
+          >
+            <img src={compShapeDet2} alt="component 2" />
+          </div>
+        </div>
+
+        <div className="flex flex-wrap justify-evenly items-center">
+          <div
+            className="flex items-center justify-center shrink "
+            data-aos="slide-right"
+          >
+            <img src={compShape3} alt="component 1" />
+          </div>
+          <div
+            className="flex items-center justify-start shrink"
+            data-aos="slide-left"
+          >
+            <img src={compShapeDet3} alt="component 2" />
+          </div>
+        </div>
+
+        <div className="flex flex-wrap justify-evenly items-center">
+          <div
+            className="flex items-center justify-center shrink "
+            data-aos="slide-right"
+          >
+            <img src={compShape4} alt="component 1" />
+          </div>
+          <div
+            className="flex items-center justify-start shrink"
+            data-aos="slide-left"
+          >
+            <img src={compShapeDet4} alt="component 2" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default ValueProposition
+export default ValueProposition;
--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Home from './pages/Home';
-import Blogs from './pages/Blogs'
-import Post from './pages/Post'
-import ContactUs from './pages/ContactUs';
-import Team from './pages/Team';
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Post from "./pages/Post";
+import ContactUs from "./pages/ContactUs";
+import Team from "./pages/Team";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import ValueProposition from "./components/sections/ValueProposition";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <NavBar/>
-    <Routes> 
-      <Route path="/" element={<Home/>}/>
-      <Route path="/blogs/:slug" element={<Post/>}/>
-      <Route path="/blogs" element={<Blogs/>}/>
-      <Route path="/contactUs" element={<ContactUs/>}/>
-      <Route path="/team" element={<Team/>}/>
+    <NavBar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/blogs/:slug" element={<Post />} />
+      <Route path="/blogs" element={<Blogs />} />
+      <Route path="/contactUs" element={<ContactUs />} />
+      <Route path="/team" element={<Team />} />
+      <Route path="/valueProposition" element={<ValueProposition />} />
     </Routes>
-    <Footer/>
+    <Footer />
   </BrowserRouter>
 );
